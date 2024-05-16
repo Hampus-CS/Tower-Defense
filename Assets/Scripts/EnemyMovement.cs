ae9f83fda3e492c46bac0d82de5656c03a0cc5c1
Hampus-CS <150423568+Hampus-CS@users.noreply.github.com>
2024-05-16 17:04:15 +0200

    Initial commit
    Adding unity project

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public List<GameObject> waypoints;
+    Rigidbody2D rb;
+    int waypointIndex = 0;
+    public float speedBoost = 1f;
+    public GameObject spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform.position = spawnPosition.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gör så att fienden går till nästa punkt
+        Vector2 direction = waypoints[waypointIndex].transform.position - transform.position;
+        rb.velocity = direction.normalized * speedBoost;
+
+        // Ändrar rotation så att fienden kollar åt hållet nästa punkt finns på
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+
+
+        if (Vector2.Distance(waypoints[waypointIndex].transform.position, transform.position) < 0.3)
+        {
+            waypointIndex += 1;
+
+            if (waypointIndex == waypoints.Count)
+            {
+                Destroy(gameObject);
+            }
+            
+
+        }
+
+    }
+}
